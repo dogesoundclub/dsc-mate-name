66dad8440b7cd124eeaba8ed4032212bba6dc14c
Hanul <hanul@hanul.me>
2021-08-19 15:31:57 +0900

    메인넷 배포 완료

--- README.md
+++ README.md
@@ -2,9 +2,7 @@
 이제 메이트들에 이름을 붙힐 수 있습니다.
 
 ## Mainnet
-
-## Baobab
-0x3e933b1E921810460ab85fB97d1262fAa47c9Bae
+0xe0A08F4e6a07015F04EACF565ab3b71C9e0dD546
 
 ## License
 [MIT](LICENSE)
--- contracts/interfaces/IDSCMateName.sol
+++ contracts/interfaces/IDSCMateName.sol
@@ -4,6 +4,7 @@ interface IDSCMateName {
     
     event Set(uint256 indexed mateId, address indexed owner, string name);
     
+    function tokenAmountForChanging() view external returns (uint256);
     function set(uint256 mateId, string calldata name) external;
     function recordCount(uint256 mateId) view external returns (uint256);
     function record(uint256 mateId, uint256 index) view external returns (address owner, string memory name, uint256 blockNumber);
